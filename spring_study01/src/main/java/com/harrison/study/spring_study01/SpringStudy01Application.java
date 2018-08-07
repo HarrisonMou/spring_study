2d6aa7ed5ed260e2c0290d4bb31051927c5238df
Harrison Mou <a847381241@163.com>
2018-08-08 00:11:56 +0800

    第一次提交

---
+++ spring_study01/src/main/java/com/harrison/study/spring_study01/SpringStudy01Application.java
@@ -0,0 +1,12 @@
+package com.harrison.study.spring_study01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudy01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudy01Application.class, args);
+	}
+}
